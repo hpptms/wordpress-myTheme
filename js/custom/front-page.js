e7044acf441dd9549048ad0f6f86ef00105dd91d
hpptms <pikatyuu4188@gmail.com>
2016-10-14 12:55:11 +0900

    jsではなくcssのメディアクエリを利用

--- js/custom/front-page.js
+++ js/custom/front-page.js
@@ -1,37 +1,37 @@
 // /*準備*/
 // // 基準とする画面サイズ
-// const pc = 1200;
-// const tablet = 991;
-// const mobile = 767;
+const pc = 1200;
+const tablet = 991;
+const mobile = 767;
 // //jqueryを短縮
-// $ = jQuery;
+$ = jQuery;
 // /*準備 end*/
-//
-// // 画面の幅を取得
-// var window_w;
-// var window_h;
-// $(window).on('load resize', function(){
-//   window_w = $(window).width();
-//   change_post_layout(window_w);
-//
-//   window_h = $(window).height();
-// });
-//
-//
-// // 画面の横幅に合わせて記事の表示数を調整
-// var change_post_layout = function(w){
-//   if(w <= pc){
-//     console.info("画面の横幅 : " + w);
-//     pc_set();
-//   }else if(w <= tablet){
-//     console.info("画面の横幅 : " + w);
-//     tablet_set();
-//   }else if(w <= mobile){
-//     console.info("画面の横幅 : " + w);
-//     mobile_set();
-//   }
-// };
-//
+
+// 画面の幅を取得
+var window_w;
+var window_h;
+$(window).on('load resize', function(){
+  window_w = $(window).width();
+  change_post_layout(window_w);
+
+  window_h = $(window).height();
+});
+
+
+// 画面の横幅に合わせて記事の表示数を調整
+var change_post_layout = function(w){
+  if(w <= pc){
+    console.info("画面の横幅 : " + w);
+    // pc_set();
+  }else if(w <= tablet){
+    console.info("画面の横幅 : " + w);
+    // tablet_set();
+  }else if(w <= mobile){
+    console.info("画面の横幅 : " + w);
+    // mobile_set();
+  }
+};
+
 // // 記事のオブジェクトを取得
 // var g_post = $(".front-loop-cont article");
 // var cnt_post = 1;
