74b43c19554b902eda1ae34fb24a1e2456d1b5ae
hpptms <pikatyuu4188@gmail.com>
2016-11-30 15:30:25 +0900

    サイズの取り方を変更

--- front-page.php
+++ front-page.php
@@ -23,72 +23,72 @@ $data_ad_client = file_get_contents('http://hpptms.dip.jp/wp-content/themes/xeor
         <div class="wrap" style="margin: 0; height:100%; width:100%">
           <div class="front-loop-cont">
             <section id="posts">
-            <?php
-            $i = 1;
-            wp_reset_query();
-            //パラメータ指定
-            $args =
-            array(
-              'meta_query'=>
-              array(
-                array('key'=>'bzb_show_toppage_flag',
-                'compare' => 'NOT EXISTS'
-              ),
-              array('key'=>'bzb_show_toppage_flag',
-              'value'=>'none',
-              'compare'=>'!='
-            ),
-            'relation'=>'OR'
-          ),
-          //記事の数
-          //'showposts'=>$published_posts,
-          'showposts'=>12,
-          'order'=>'DESC'
-        );
-        query_posts($args);
-        //query_posts('showposts=5&order=DESC');
-        if ( have_posts() ) :
-          while ( have_posts() ) : the_post();
-          $cf = get_post_meta($post->ID);
-          $recent_class = 'popular_post_box recent-'.$i;
-          ?>
-
-          <article id="post-<?php echo the_ID(); ?>" <?php post_class($recent_class); ?> >
-            <a href="<?php the_permalink(); ?>" class="wrap-a"><?php if( get_the_post_thumbnail() ) { ?>
-              <div class="post-thumbnail post-thumbnail-r-custom">
-                <?php the_post_thumbnail(array('300','158'));
-                ?>
-              </div>
-              <?php } else{ ?>
-                <img src="<?php echo get_template_directory_uri(); ?>/lib/images/noimage.jpg" alt="noimage" width="300" height="158" />
-                <?php } // get_the_post_thumbnail ?>
-                <p class="p_category"><?php $cat = get_the_category(); $cat = $cat[0]; {echo $cat->cat_name;} ?></p>
-                <h3 class="h3_title" style="margin-bottom:0;"><?php the_title(); ?></h3>
-                <p class="p_date"><span class="date-y"><?php the_time('Y'); ?></span><span class="date-mj"><?php the_time('m/j'); ?></span></p></a>
-              </article>
-
               <?php
-              $i++;
-            endwhile;
-          endif;
-          ?>
+              $i = 1;
+              wp_reset_query();
+              //パラメータ指定
+              $args =
+              array(
+                'meta_query'=>
+                array(
+                  array('key'=>'bzb_show_toppage_flag',
+                  'compare' => 'NOT EXISTS'
+                ),
+                array('key'=>'bzb_show_toppage_flag',
+                'value'=>'none',
+                'compare'=>'!='
+              ),
+              'relation'=>'OR'
+            ),
+            //記事の数
+            //'showposts'=>$published_posts,
+            'showposts'=>12,
+            'order'=>'DESC'
+          );
+          query_posts($args);
+          //query_posts('showposts=5&order=DESC');
+          if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+            $cf = get_post_meta($post->ID);
+            $recent_class = 'popular_post_box recent-'.$i;
+            ?>
 
-          <p>スポンサーリンク</p>
-          <!--広告を表示-->
-          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <!-- Google AdSence -->
-          <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client=<?php echo $data_ad_client ?>
-          data-ad-slot=<?php echo $data_ad_slot ?>
-          data-ad-format="auto"></ins>
-          <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>
+            <article id="post-<?php echo the_ID(); ?>" <?php post_class($recent_class); ?> >
+              <a href="<?php the_permalink(); ?>" class="wrap-a"><?php if( get_the_post_thumbnail() ) { ?>
+                <div class="post-thumbnail post-thumbnail-r-custom">
+                  <?php the_post_thumbnail(array('300','158'));
+                  ?>
+                </div>
+                <?php } else{ ?>
+                  <img src="<?php echo get_template_directory_uri(); ?>/lib/images/noimage.jpg" alt="noimage" width="300" height="158" />
+                  <?php } // get_the_post_thumbnail ?>
+                  <p class="p_category"><?php $cat = get_the_category(); $cat = $cat[0]; {echo $cat->cat_name;} ?></p>
+                  <h3 class="h3_title" style="margin-bottom:0;"><?php the_title(); ?></h3>
+                  <p class="p_date"><span class="date-y"><?php the_time('Y'); ?></span><span class="date-mj"><?php the_time('m/j'); ?></span></p></a>
+                </article>
 
-          <?php
-          ?>
-        </section><!-- /posts -->
+                <?php
+                $i++;
+              endwhile;
+            endif;
+            ?>
+
+            <p id="Advertising">スポンサーリンク</p>
+            <!--広告を表示-->
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- Google AdSence -->
+            <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client=<?php echo $data_ad_client ?>
+            data-ad-slot=<?php echo $data_ad_slot ?>
+            data-ad-format="auto"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+
+            <?php
+            ?>
+          </section><!-- /posts -->
         </div><!-- /front-root-cont -->
       </div><!-- /wrap -->
     </div><!-- /recent_post_content -->
--- js/custom/front-page.js
+++ js/custom/front-page.js
@@ -1,33 +1,70 @@
 /*準備*/
+// 基準とする画面サイズ
+//const pc = 1200;
+const tablet = 1199;
+const mobile = 767;
 $ = jQuery;
 /*準備 end*/
 
 // 画面の幅を取得
 var window_h;
+var window_w;
 
+/* ページ読み込み後 **************/
 $(window).load(function(){
-  height_check(window_h);
+  window_w = $(window).width();
+  width_check(window_w);
 });
 
-// ページ表示後
+/* ページ表示後  ****************/
 var timer = false;
 $(window).resize(function() {
   if(timer !== false){
     clearTimeout(timer);
   }
   timer = setTimeout(function() {
-    height_check(window_h);
+    window_w = $(window).width();
+    width_check(window_w);
+    console.log('resized');
   }, 200);
 });
 
-
 // 画面の横幅に合わせて記事の表示数を調整
-var height_check = function(w){
-  window_h = $('#posts').outerHeight(true);
-  height_set();
+var width_check = function(w){
+  if(w < mobile){
+    mobile_set();
+  }else if(w < tablet){
+    tablet_set();
+  }else{
+    pc_set();
+  }
 };
 
-var height_set = function(){
-  $('#posts').css('height', window_h +150 + 'px');
-  $("#content").css("height",$(".front-loop-cont").outerHeight(true));
+//サイズを調べる
+var article_height;
+var article_num;
+var Advertising;
+var get_article = function(){
+  article_height = $('article').outerHeight(true);
+  article_num = $('article').length;
+  Advertising = $('.adsbygoogle').outerHeight(true);
+}
+
+// PC用の設定 1200以上
+var pc_set = function(){
+  get_article();
+  $('#posts').css('height', ((article_num / 4 ) * article_height + Advertising + 100 ) + 'px');
+   $("#content").css("height",$("#posts").outerHeight(true));
+};
+// tablet用の設定 991
+var tablet_set = function(){
+  get_article();
+  $('#posts').css('height', ((article_num / 3 ) * article_height + Advertising + 100 ) + 'px');
+  $('#content').css('height',$('#posts').outerHeight(true));
+};
+// mobile用の設定 767
+var mobile_set = function(){
+  get_article();
+  $('#posts').css('height', (article_num * article_height + Advertising + 100 ) + 'px');
+  $('#content').css('height',$('#posts').outerHeight(true));
 };
